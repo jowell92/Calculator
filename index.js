4c8ba5a61cba647b94a6b23dcdec12e47ed57550
Joel <jr92study@gmail.com>
2020-12-28 01:19:18 +0100

    files

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+//function suma
+function suma() {
+    let num1=document.getElementById("num1").value;
+    let num2=document.getElementById("num2").value;
+
+    let suma=parseInt(num1)+parseInt(num2);
+
+    window.alert(suma);
+
+}
